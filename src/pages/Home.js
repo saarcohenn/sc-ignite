785ea1f777a53f723254ae52cb935455bd4eff78
saar cohen <saar.cohenn@gmail.com>
2021-01-24 16:55:14 +0200

    [Added] - Game Cards and styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,76 @@
 import React, { useEffect } from "react";
 // Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+// Components
+import Game from "../components/Game";
+// styling and animations
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  }, []);
+  }, [dispatch]);
 
+  const { popularGames, newGames, upcomingGames } = useSelector(
+    (state) => state.games
+  );
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcomingGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>Popular Games</h2>
+      <Games>
+        {popularGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
